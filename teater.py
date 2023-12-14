38cac1c3619ad98ac98d9e38e402c5fc007f0438
BLACKTHELEGEND <113369444+BLACKTHELEGEND@users.noreply.github.com>
2023-12-14 11:50:10 +0100

    Update teater.py

--- teater.py
+++ teater.py
@@ -7,6 +7,6 @@ class Person:
 
 class Biletter(Person):
     def __init__(self, Navn, Alder, telefonNr, Epost, stykke:str, antallBiletter:int=1)
-        super()__init__(Navn, Alder, telefonNr, Epost)
+        super().__init__(Navn, Alder, telefonNr, Epost)
         self.stykke = stykke
         self.antallBiletter = antallBiletter
