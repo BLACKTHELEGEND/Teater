8d5580ca11a9f860875f5a6ace15891135dd166d
shaunmwandh <shaunmwandha@gmail.com>
2023-12-14 11:03:59 +0100

    Preson class

--- teater.py
+++ teater.py
@@ -1,1 +1,7 @@
 kult
+class Person:
+    def __init__(self, Navn, Alder, telefon, Epost):
+        self.Navn = Navn
+        self.Alder = Alder
+        self.telefon = telefon
+        self.Epost = Epost
