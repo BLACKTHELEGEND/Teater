0e6d89ed9a222574efda8db8fc7cb6ce90b91fe2
WienerDoug <153815853+WienerDoug@users.noreply.github.com>
2023-12-20 08:52:58 +0100

    Update teater.py

--- teater.py
+++ teater.py
@@ -4,7 +4,7 @@ class Person:
         self.Alder = Alder 
         self.telefonNr = telefonNr 
         self.Epost = Epost
-
+ 
 class Biletter(Person):
     def __init__(self, Navn, Alder, telefonNr, Epost, stykke:str, antallBiletter:int=1):
         super().__init__(Navn, Alder, telefonNr, Epost)
